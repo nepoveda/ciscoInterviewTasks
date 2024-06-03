12ad132cdfd169bc094db526de922cb93d78bacc
Dan Nepejchal <dan.nepejchal@european-pirateparty.eu>
2024-06-03 18:03:43 +0200

    Task no. 3 words count in file

---
+++ words_count/word_count.py
@@ -0,0 +1,15 @@
+import string
+
+words_count = dict()
+with open("text.txt", "r") as text_file:
+    for line in text_file:
+        # strip lines, lower text,
+        words = line.strip().lower().translate(line.maketrans("", "", string.punctuation)).split(" ")
+        for word in words:
+            if word in words_count:
+                words_count[word] += 1
+            else:
+                words_count[word] = 1
+
+for word in list(words_count.keys()):
+    print(f"{word}: {words_count[word]}")
